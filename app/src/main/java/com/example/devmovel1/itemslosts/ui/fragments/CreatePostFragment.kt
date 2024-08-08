816335fd50e212fb72468a6913641174d42f4015
Igor-Ueno <kenjiueno0011@gmail.com>
2024-08-08 14:06:28 -0300

    Adaptação de alguns arquivos

--- app/src/main/java/com/example/devmovel1/itemslosts/ui/fragments/CreatePostFragment.kt
+++ app/src/main/java/com/example/devmovel1/itemslosts/ui/fragments/CreatePostFragment.kt
@@ -10,6 +10,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.devmovel1.R
 import com.example.devmovel1.itemslosts.services.PostsService
+import com.example.devmovel1.itemslosts.services.SessionManager
 import com.example.itemslosts.models.LostItem
 import retrofit2.Call
 import retrofit2.Callback
@@ -17,8 +18,8 @@ import retrofit2.Response
 
 class CreatePostFragment : Fragment() {
 
-    private val token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjZhYTgxN2IwNDUyZDBjOWY4NTZiZmM1In0sImlhdCI6MTcyMjUxNDQ0NSwiZXhwIjoxNzIyNTE4MDQ1fQ.sfQrOVKZWEiYfVAO3-6uTkFRUmwjhZeYGj4yGWJdAb0"
-
+    private lateinit var sessionManager: SessionManager
+    private var token = ""
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -28,6 +29,8 @@ class CreatePostFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        sessionManager = SessionManager(requireContext())
+        token = sessionManager.fetchToken().toString()
 
         val editTextTitle: EditText = view.findViewById(R.id.editTextTitle)
         val editTextDescription: EditText = view.findViewById(R.id.editTextDescription)
